7cfd41053694d46b4a438430066306181b38a3bc
Mohammed Mudassir <mdmudassirpro@gmail.com>
2020-01-01 21:09:16 +0530

    2nd challenge

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,10 @@
-function add(param1: number, param2: number): number {
+const add = (param1: number, param2: number): number => param1 + param2;
 
-}
+const add2 = (...param1: number[]): number =>
+  param1.reduce((acc, val) => acc + val);
 
-// function add2(param1: number[]): number {
-
-// }
-// console.log(add(1, 2));
+console.log(add(1, 6));
 console.log(add(3, 2));
 
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
-
+console.log(add2(1, 2, 3, 4, 5));
+console.log(add2(2, 3));
