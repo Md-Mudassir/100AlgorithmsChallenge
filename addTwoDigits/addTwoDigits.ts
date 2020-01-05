9c6dda80b37cb3c4f524e557a7d9ca344c98132b
Mohammed Mudassir <mdmudassirpro@gmail.com>
2020-01-05 15:38:25 +0530

    5th day

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,8 @@
-function addTwoDigits(n: any): number {
+const addTwoDigits = (n: any): number =>
+  n
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce((a, b) => a + b);
 
-}
-
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(291112));
