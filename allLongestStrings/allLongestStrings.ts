0076ea94adf5617ff93025374b2f1dc9f2461b29
Mohammed Mudassir <mdmudassirpro@gmail.com>
2020-01-07 20:17:29 +0530

    refactored

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,14 +1,12 @@
 const allLongestStrings = (inputArray: string[]): string[] => {
   let longestWord = 0;
-  let longestWords = [];
 
-  inputArray.forEach((word: string) => {
-    longestWord = longestWord < word.length ? word.length : longestWord;
-  });
+  inputArray.forEach(
+    (word: string) =>
+      (longestWord = longestWord < word.length ? word.length : longestWord)
+  );
 
-  return (longestWords = inputArray.filter(
-    words => words.length === longestWord
-  ));
+  return inputArray.filter(words => words.length === longestWord);
 };
 
 console.log(allLongestStrings(['aba', 'aa', 'ad', 'vcd', 'aba']));
